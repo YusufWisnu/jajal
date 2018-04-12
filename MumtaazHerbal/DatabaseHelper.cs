2b7b37656701e6d123631e6667073f6b715b3f7d
YusufWisnu <yusufnagashi@ymail.com>
2018-04-12 11:23:57 +0700

    masih ke database

---
+++ MumtaazHerbal/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace MumtaazHerbal
+{
+    class DatabaseHelper
+    {
+        //Database File name
+        public string DatabaseName = "Mumtaaz1db.db";
+
+        public string GetConnection()
+        {
+            return @"Data Source=" + DatabaseName + "; Version=3";
+        }
+
+        public void CreateDatabase()
+        {
+            using(SQLiteConnection con = new SQLiteConnection())
+            {
+                SQLiteCommand cmd = new SQLiteCommand();
+
+            }
+            {
+
+            }
+        }
+    }
+}
